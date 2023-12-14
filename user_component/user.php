4afd14d85b9d829594be665ecd060a6a7f9557bd
Buffett Liu <buffettliu@BuffettMBP.local>
2023-12-14 12:38:48 +0800

    User Optimization 1214

--- user_component/user.php
+++ user_component/user.php
@@ -14,17 +14,22 @@
 
 <body>
     <div class="container">
-        <h1>Welcome Hooper's User</h1>
-        <li class="user-info"><?php echo isset($_SESSION['username']) ? 'Welcome, ' . $_SESSION['username'] : 'No User Login'; ?></li>
-        <div class="link-container">
+            <h1>Welcome Hooper's User</h1>
             <a href="register.php" class="search-link">使用者註冊</a>
             <a href="login.php" class="search-link">使用者登入</a>
+            <a href="logout.php" class="search-link">使用者登出</a>
+            <!-- <a href="register.php" class="register-button">使用者註冊</a>
+            <a href="login.php" class="login-button">使用者登入</a> -->
+            <li class="user-info"><?php echo isset($_SESSION['username']) ? 'Welcome, ' . $_SESSION['username'] : 'No User Login'; ?></li>
+
+    </div>
+    <div class="container">
+            <!-- <a href="register.php" class="search-link">使用者註冊</a>
+            <a href="login.php" class="search-link">使用者登入</a> -->
             <a href="match_search.php" class="search-link">參與約戰</a>
             <a href="court_search.php" class="search-link">場地搜索</a>
             <a href="court_display.php" class="search-link">場地查看</a>
             <a href="review.php" class="search-link">評價系統</a>
-            <a href="logout.php" class="search-link">使用者登出</a>
-        </div>
     </div>
 
     <div class="container">
