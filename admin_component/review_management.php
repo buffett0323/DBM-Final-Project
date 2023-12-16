a58d6974d792449ebccfcba8c286d35bd77a4223
Buffett Liu <buffettliu@BuffettMBP.local>
2023-12-16 13:32:58 +0800

    Admin function last push 1216

--- admin_component/review_management.php
+++ admin_component/review_management.php
@@ -1,71 +1,123 @@
 <?php
-// This page is only be accessible to logged-in users, add a check at the beginning.
-session_start();
+    // This page is only be accessible to logged-in users, add a check at the beginning.
+    session_start();
 
-if (!isset($_SESSION['admin_name'])) {
-    header('Location: login.php');
-    exit;
-}
+    if (!isset($_SESSION['admin_name'])) {
+        header('Location: login.php');
+        exit;
+    }
 
-$host = 'localhost';
-$port = 5432;
-$dbname = 'hooper';
-$user = 'postgres';
-$password = trim(file_get_contents('../db_password.txt'));
+    $host = 'localhost';
+    $port = 5432;
+    $dbname = 'hooper';
+    $user = 'postgres';
+    $password = trim(file_get_contents('../db_password.txt'));
 
-// pdo settings
-$pdo = null;
-try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Database connection failed: " . $e->getMessage();
-}
+    // pdo settings
+    $pdo = null;
+    try {
+        $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Database connection failed: " . $e->getMessage();
+    }
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>match_search</title>
+    <title>Review Management</title>
     <link rel="stylesheet" href="user.css">
 </head>
 
 <body>
     <div class="container">
         <h1>Review Record Search</h1>
-
         <form action="review_management.php" method="post">
             評論者ID: <input type="text" name="reviewerid" required>
-            <button type="submit">Search</button>
+            <button type="submit" name="search_submit">Search</button>
         </form>
 
         <?php
 
         try {
-            // Check if the form is submitted
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                // Get the username from the form
-                $searchReviewerid = $_POST['reviewer'];
-                echo $searchReviewerid;
+            $result = [];
+            // Check if the form is submitted with the "Search" button
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['search_submit'])) {
+                // Get the reviewerid from the form
+                $reviewerid = $_POST['reviewerid'];
                 // Use a prepared statement to prevent SQL injection
-                $stmt = $pdo->prepare('Delete from review
-                                        WHERE reviewerid = :reviewerid');
-                $stmt->bindParam(':reviewerid', $Reviewerid);
+                $stmt = $pdo->prepare('SELECT * 
+            FROM review as r 
+            WHERE r.reviewerid = :reviewerid
+            ORDER BY reviewerid, revieweeid');
+                $stmt->bindParam(':reviewerid', $reviewerid, PDO::FETCH_ASSOC);
                 $stmt->execute();
-                echo 'Deleted obnoxious comments';
-
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             }
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
-        } finally {
-            // Close the database connection
-            $pdo = null;
         }
         ?>
 
-    </div>
-    <div class="container">
+        <?php
+        // Delete record if delete button is clicked
+        if (isset($_POST['delete_reviewerid']) && isset($_POST['delete_revieweeid'])) {
+            $deleteReviewerId = $_POST['delete_reviewerid'];
+            $deleteRevieweeId = $_POST['delete_revieweeid'];
+            $sql = "DELETE FROM review 
+                WHERE reviewerid = :reviewerid 
+                AND revieweeid = :revieweeid";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':reviewerid', $deleteReviewerId, PDO::PARAM_STR);
+            $stmt->bindParam(':revieweeid', $deleteRevieweeId, PDO::PARAM_STR);
+            $stmt->execute();
+            echo "<h3>Successfully Delete!<h3>";
+        }
+        ?>
+
+
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Reviewer ID</th>
+                    <th>Reviewee ID</th>
+                    <th>Comment</th>
+                    <th>Rating</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($result as $review): ?>
+                    <tr>
+                        <td>
+                            <?= isset($review['reviewerid']) ? $review['reviewerid'] : 'N/A' ?>
+                        </td>
+                        <td>
+                            <?= isset($review['revieweeid']) ? $review['revieweeid'] : 'N/A' ?>
+                        </td>
+                        <td>
+                            <?= $review['comment'] ?? 'N/A' ?>
+                        </td>
+                        <td>
+                            <?= $review['rating'] ?? 'N/A' ?>
+                        </td>
+                        <td>
+                            <form method="post">
+                                <input type="hidden" name="delete_reviewerid"
+                                    value="<?= isset($review['reviewerid']) ? $review['reviewerid'] : '' ?>">
+                                <input type="hidden" name="delete_revieweeid"
+                                    value="<?= isset($review['revieweeid']) ? $review['revieweeid'] : '' ?>">
+                                <button class="delete-button" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
         <!-- Button to go back to the previous page and main page (index page) -->
         <button class="button previous" onclick="history.back();">Previous Page</button>
         <button class="button main" onclick="window.location.href='../index.php';">Main Page</button>
--- admin_component/user_record_search.php
+++ admin_component/user_record_search.php
@@ -1,93 +1,86 @@
 <?php
-// This page is only be accessible to logged-in users, add a check at the beginning.
-session_start();
+    // This page is only be accessible to logged-in users, add a check at the beginning.
+    session_start();
 
-if (!isset($_SESSION['admin_name'])) {
-    header('Location: login.php');
-    exit;
-}
+    if (!isset($_SESSION['admin_name'])) {
+        header('Location: login.php');
+        exit;
+    }
 
-$host = 'localhost';
-$port = 5432;
-$dbname = 'hooper';
-$user = 'postgres';
-$password = trim(file_get_contents('../db_password.txt'));
+    $host = 'localhost';
+    $port = 5432;
+    $dbname = 'hooper';
+    $user = 'postgres';
+    $password = trim(file_get_contents('../db_password.txt'));
 
-// pdo settings
-$pdo = null;
-try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Database connection failed: " . $e->getMessage();
-}
+    // pdo settings
+    $pdo = null;
+    try {
+        $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Database connection failed: " . $e->getMessage();
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>match_search</title>
+    <title>User Record</title>
     <link rel="stylesheet" href="user.css">
 </head>
 
 <body>
     <div class="container">
-        <h1>User Record Search</h1>
+        <h1>Inquiring User Record</h1>
 
         <form action="user_record_search.php" method="post">
-            使用者名稱: <input type="text" name="username" required>
+            使用者ID: <input type="text" name="userid" required>
             <button type="submit">Search</button>
         </form>
 
+        <button class="button previous" onclick="history.back();">Previous Page</button>
+        <button class="button main" onclick="window.location.href='../index.php';">Main Page</button>
+        
         <?php
-
         try {
             // Check if the form is submitted
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Get the username from the form
-                $searchUsername = $_POST['username'];
+                $userid = $_POST['userid'];
                 // Use a prepared statement to prevent SQL injection
-                $stmt = $pdo->prepare('Select u.userid, u.username, u.level, m.matchid as holdingid, p.matchid as participatingid
-                                    From users as u
-                                    Left join match as m on m.holderid = u.userid
-                                    Left join participate as p on p.userid = u.userid
-                                    WHERE username = :username');
-                $stmt->bindParam(':username', $searchUsername);
+                $stmt = $pdo->prepare('SELECT u.userid, u.username, u.level, m.matchid as holding_match, p.matchid as participate_match
+                    FROM users as u
+                    LEFT JOIN match as m on m.holderid = u.userid
+                    LEFT JOIN participate as p on p.userid = u.userid
+                    WHERE u.userid = :userid');
+                $stmt->bindParam(':userid', $userid);
                 $stmt->execute();
-
-                // Fetch and display the results
                 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+                // Display the result
                 if ($result) {
-                    echo '<h2>Search Results:</h2>';
-                    echo '<h3>Holding Record:</h3>';
-                    echo '<ul>';
+                    // echo "<h2>User Record</h2>";
+                    echo "<table>";
+                    echo "<tr><th>User ID</th><th>User Name</th><th>User Level</th><th>Holding Match</th><th>Participate Match</th></tr>";
                     foreach ($result as $row) {
-                        echo '<li>Userid: ' . $row['userid'] . ', Username: ' . $row['username'] . ', Userlevel: ' . $row['level'] . ', Matchid: ' . $row['holdingid'] . ' </li>';
+                        echo "<tr>";
+                        echo "<td>" . htmlspecialchars($row['userid']) . "</td>";
+                        echo "<td>" . htmlspecialchars($row['username']) . "</td>";
+                        echo "<td>" . htmlspecialchars($row['level']) . "</td>";
+                        echo "<td>" . htmlspecialchars($row['holding_match']) . "</td>";
+                        echo "<td>" . htmlspecialchars($row['participate_match']) . "</td>";
+                        echo "</tr>";
                     }
-                    echo '<h3>Participating Record:</h3>';
-                    foreach ($result as $row) {
-                        echo '<li>Userid: ' . $row['userid'] . ', Username: ' . $row['username'] . ', Userlevel: ' . $row['level'] . ', Matchid: ' . $row['participatingid'] . ' </li>';
-                    }
-                    echo '</ul>';
                 } else {
-                    echo '<p>No records found for the specified username.</p>';
+                    echo "<p>No records found matching your input ID.</p>";
                 }
             }
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
-        } finally {
-            // Close the database connection
-            $pdo = null;
         }
         ?>
-
-    </div>
-    <div class="container">
-        <!-- Button to go back to the previous page and main page (index page) -->
-        <button class="button previous" onclick="history.back();">Previous Page</button>
-        <button class="button main" onclick="window.location.href='../index.php';">Main Page</button>
     </div>
 </body>
 
